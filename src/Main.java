d857d17b71df9cb770e10e4853a13ccc6dbb80ee
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-01-15 20:23:38 +0300

    cycleWhile-task1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("ЗАДАНИЕ1");
+        int sobrat= 2_459_000;
+        int otlozit = 15_000;
+        int total = 0;
+        int moth=1;
+        while (total<sobrat){
+total=total+otlozit;
+            System.out.println ("месяц"+moth+"умма накоплений"+total+"рублей");
+            moth++;
+
+
+
+
+        }
+
+
+    }
+}
